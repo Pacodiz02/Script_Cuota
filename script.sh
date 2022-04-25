7aedfbffc851471a717474b1fdaad81d933057b6
Pacodiz02 <73858527+Pacodiz02@users.noreply.github.com>
2022-04-25 13:52:06 +0200

    cambios

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-#!usr/bin/env bash
+#!/usr/bin/env bash
 #Zona de declaración de variables
 
 libreria=$(cd $(dirname "${BASH_SOURCE[0]}") >/dev/null && pwd)
@@ -7,14 +7,16 @@ source $libreria/funciones
 DIRECTORIO="/QUOTA"
 DISPOSITIVO="sda1"
 REPO="ftp.es.debian.org"
+BINARIO="quota"
 
 #Zona de declaración de funciones
 f_limpia
 f_root
+f_conexion $REPO
+f_instalarPaquete
 f_existe_directorio $DIRECTORIO
 f_UUID $DISPOSITIVO
 f_modifica_fstab
-f_conexion $REPO
 f_habilita_cuota
 f_plantilla_cuota
 f_configura_cuota $DIRECTORIO
