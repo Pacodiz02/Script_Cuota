53f7bf52335c8a9ccd8702c4e96aacfa734b3f07
angelsuarez1612 <98739735+angelsuarez1612@users.noreply.github.com>
2022-04-30 16:36:12 +0200

    Funciones y script terminado

--- script.sh
+++ script.sh
@@ -9,6 +9,9 @@ f_root
 f_conexion
 f_instalaPaquete
 f_existe_directorio
+f_mostrar_lsblk
+read -p "Introduce el dispositivo sobre el que aplicar la cuota: " dispositivo
+uuid=$(blkid -s UUID -o value /dev/$dispositivo)
 f_UUID
 f_modifica_fstab
 f_habilita_quota
